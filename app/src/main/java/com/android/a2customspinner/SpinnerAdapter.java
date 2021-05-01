dd667e0fd2fe14eed15bfac3734f2e344b52953f
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 10:05:52 +0700

    custom item

--- app/src/main/java/com/android/a2customspinner/SpinnerAdapter.java
+++ app/src/main/java/com/android/a2customspinner/SpinnerAdapter.java
@@ -2,10 +2,13 @@ package com.android.a2customspinner;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -19,7 +22,7 @@ public class SpinnerAdapter extends BaseAdapter {
     public SpinnerAdapter(List<String> listData, Activity activity) {
         this.listData = listData;
         this.activity = activity;
-        this.layoutInflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -40,11 +43,27 @@ public class SpinnerAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         View view1 = view;
-        if (view == null){
-            view1 = layoutInflater.inflate(R.layout.spinner_item,null);
+        if (view == null) {
+            view1 = layoutInflater.inflate(R.layout.spinner_item, null);
         }
         TextView tv = (TextView) view1.findViewById(R.id.textview);
         tv.setText(listData.get(i));
         return view1;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        View view = super.getDropDownView(position, convertView, parent);
+        LinearLayout linearLayout = (LinearLayout) view;
+        TextView textView = (TextView) linearLayout.findViewById(R.id.textview);
+        textView.setGravity(Gravity.LEFT);
+        textView.setTextColor(Color.parseColor("#FF03DAC5"));
+        textView.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+        );
+
+        return view;
     }
 }
